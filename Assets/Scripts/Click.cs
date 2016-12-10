64a40037970775baf9d97221d2bd74723f7e786b
Lee-JaeGu <201511163@sangmyung.kr>
2016-12-10 19:57:22 +0900

    2016-12-10

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public UnityEngine.UI.Text gpc;
+    public UnityEngine.UI.Text goldDisplay;
+    public float gold = 0.00f;
+    public int goldperclick = 1;
+
+    void Update()
+    {
+        goldDisplay.text = "Gold: " + gold.ToString ("F0");
+        gpc.text = goldperclick + " gold/click";
+    }
+
+    public void Clicked()
+    {  
+        gold += goldperclick;
+    }
+}
---
+++ Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeManager : MonoBehaviour {
+
+    public Click click;
+    public UnityEngine.UI.Text itemInfo;
+    public float cost;
+    public int count = 0;
+    public int clickPower;
+    public string itemName;
+    public Color standard;
+    public Color affordable;
+    private float baseCost;
+
+    private void Start()
+    {
+        baseCost = cost;
+
+    }
+    private void Update()
+    {
+        itemInfo.text = itemName + "\nCost: " + cost + "\nPower: +" + clickPower;
+
+        if(click.gold >= cost)
+        {
+            GetComponent<Image>().color = affordable;
+        } else {
+            GetComponent<Image>().color = standard;
+        }
+    }
+
+    public void PurchaseUpgrade()
+    {
+        if(click.gold >= cost)
+        {
+            click.gold -= cost;
+            count += 1;
+            click.goldperclick += clickPower;
+            cost = Mathf.Round (baseCost * Mathf.Pow(1.15f, count));
+  
+        }
+    }
+}
